e12f86c995fce16493b3ab8c65fc71099d92798c
Deramecourt Mathurin <contact@mderam.com>
2022-02-14 14:24:58 +0100

    initial project react typescript next

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+//TODO lookup import Head from 'next/head'
+//TODO lookup import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Index: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Index;
