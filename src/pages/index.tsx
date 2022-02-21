5eb96c3c36e02dc2a15dcffabdadfcd3dd21b132
Deramecourt Mathurin <contact@mderam.com>
2022-02-21 11:50:55 +0100

    feat(index): index page showing all articles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
-//TODO lookup import Head from 'next/head'
-//TODO lookup import Image from 'next/image'
-import { withUrqlClient } from 'next-urql';
-import createUrqlClient from '../utils/createUrqlClient';
+import React from "react";
+import { useArticlesQuery } from "../generated/graphql";
+import ArticleCard from "../components/ArticleCard";
+import createUrqlClient from "../utils/createUrqlClient";
+import { withUrqlClient } from "next-urql";
 
 const Index: React.FC = () => {
+    const [{ data }] = useArticlesQuery();
+
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>Mderam Blog</h1>
+            <div>
+                {data?.articles
+                    ? data.articles.map(article =>
+                        <div key={article.id}>
+                            <ArticleCard
+                                { ...article }
+                                link={`/article/${article.slug}`}
+                            />
+                        </div>
+                    )
+                    : <p>An error occured while loading articles, please try again later...</p>
+                }
+            </div>
         </div>
-    )
+    );
 }
 
-export default withUrqlClient(createUrqlClient)(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
