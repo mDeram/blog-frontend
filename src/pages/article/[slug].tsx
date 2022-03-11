ffe4b0906790d1104a107b46b593930a1b48df13
Deramecourt Mathurin <contact@mderam.com>
2022-03-11 06:31:50 +0100

    feat: increment static regeneration for the article pages

--- src/pages/article/[slug].tsx
+++ src/pages/article/[slug].tsx
@@ -1,6 +1,6 @@
-import { GetServerSideProps, NextPage } from "next";
+import { GetServerSideProps, GetStaticPaths, GetStaticProps, NextPage } from "next";
 import React from "react";
-import { ArticleBySlugDocument, useArticleBySlugQuery } from "../../generated/graphql";
+import { ArticleBySlugDocument, ArticlesPublishedDocument, useArticleBySlugQuery } from "../../generated/graphql";
 import ArticleComponent from "../../components/Article";
 import createUrqlClient from "../../utils/createUrqlClient";
 import { initUrqlClient, withUrqlClient } from "next-urql";
@@ -19,10 +19,41 @@ const Article: NextPage<{ slug: string }> = ({ slug }) => {
     );
 }
 
-export const getServerSideProps: GetServerSideProps = async(context) => {
-    let slug = "";
-    if (typeof context.params?.slug === "string")
-        slug = context.params?.slug;
+export const getStaticPaths: GetStaticPaths = async () => {
+    const ssrCache = ssrExchange({ isClient: false });
+    const client = initUrqlClient({
+        url: "http://localhost:7000/graphql",
+        exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
+    }, false);
+
+    const articles = await client?.query(ArticlesPublishedDocument).toPromise();
+    const paths = articles?.data.articlesPublished
+        .filter((article: any) => article.slug !== "")
+        .map((article: any) => {
+        return {
+            params: {
+                slug: article.slug
+            }
+        }
+    });
+
+    return {
+        paths,
+        fallback: "blocking"
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    if (!params?.slug || typeof params.slug !== "string") {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: true
+            }
+        }
+    }
+
+    const slug = params.slug;
 
     const ssrCache = ssrExchange({ isClient: false });
     const client = initUrqlClient({
