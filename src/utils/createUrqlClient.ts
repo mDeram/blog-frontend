20115cb6675e9c999f9944e2d44ce51611494931
Deramecourt Mathurin <contact@mderam.com>
2022-04-14 17:27:56 +0200

    dev: add @urql/devtools for easier debugging

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,93 +1,102 @@
-import { dedupExchange, fetchExchange } from "urql";
+import { ClientOptions, dedupExchange, fetchExchange } from "urql";
 import { cacheExchange } from "@urql/exchange-graphcache";
 import schema, { Article, ArticlesDocument, ArticlesQuery, LikeDocument } from "../generated/graphql";
 import { NextUrqlClientConfig } from "next-urql";
 import { transformToDate } from "../cache/transformToDate";
+import { devtoolsExchange } from "@urql/devtools";
+import { ___prod___ } from "../constants";
 
-const createUrqlClient: NextUrqlClientConfig = (ssrExchange) => ({
-    url: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || "",
-    fetchOptions: {
-        credentials: "include"
-    },
-    exchanges: [
-        dedupExchange,
-        cacheExchange({
-            schema,
-            resolvers: {
-                Article: {
-                    updatedAt: transformToDate,
-                    createdAt: transformToDate,
+const createUrqlClient: NextUrqlClientConfig = (ssrExchange) => {
+    const config: ClientOptions = {
+        url: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || "",
+        fetchOptions: {
+            credentials: "include"
+        },
+        exchanges: [
+            dedupExchange,
+            cacheExchange({
+                schema,
+                resolvers: {
+                    Article: {
+                        updatedAt: transformToDate,
+                        createdAt: transformToDate,
+                    },
+                    Query: {
+                        article: (_parent, args, _cache, _info) => {
+                            return {
+                                __typename: "Article",
+                                id: args.id
+                            };
+                        }
+                    }
                 },
-                Query: {
-                    article: (_parent, args, _cache, _info) => {
-                        return {
-                            __typename: "Article",
-                            id: args.id
-                        };
+                updates: {
+                    Mutation: {
+                        createArticle: (result, _args, cache, _info) => {
+                            if (!result.createArticle) return;
+
+                            cache.updateQuery<ArticlesQuery>({ query: ArticlesDocument }, data => {
+                                if (!data) return null;
+
+                                data.articles = [result.createArticle as Article, ...data.articles];
+                                return data;
+                            });
+                        },
+                        updateArticle: (result, args, cache, _info) => {
+                            if (!result.updateArticle) return;
+
+                            cache.invalidate("Query", "articles");
+                            cache.invalidate("Query", "article", { id: args.id });
+                        },
+                        deleteArticle: (result, args, cache, _info) => {
+                            if (!result.deleteArticle) return;
+
+                            cache.updateQuery<ArticlesQuery>({ query: ArticlesDocument }, data => {
+                                if (!data) return null;
+
+                                const filteredArticles = data.articles.filter(article => article.id !== args.id);
+                                return { articles: filteredArticles } as ArticlesQuery;
+                            });
+                        },
+                        setPublishArticle: (result, args, cache, _info) => {
+                            if (!result.setPublishArticle) return;
+
+                            cache.updateQuery<ArticlesQuery>({ query: ArticlesDocument }, data => {
+                                if (!data) return null;
+
+                                const resultArticle = data.articles.find(article => article.id === args.id) as Article;
+                                resultArticle.published = args.published as boolean;
+                                return data;
+                            });
+                        },
+                        toggleLike: (result, args, cache, _info) => {
+                            if (!result.toggleLike) return;
+
+                            cache.updateQuery({ query: LikeDocument, variables: { articleId: args.articleId } }, data => {
+                                if (!data) return null;
+
+                                data.like = !data.like;
+                                return data;
+                            });
+                        }
                     }
                 }
-            },
-            updates: {
-                Mutation: {
-                    createArticle: (result, _args, cache, _info) => {
-                        if (!result.createArticle) return;
-
-                        cache.updateQuery<ArticlesQuery>({ query: ArticlesDocument }, data => {
-                            if (!data) return null;
-
-                            data.articles = [result.createArticle as Article, ...data.articles];
-                            return data;
-                        });
-                    },
-                    updateArticle: (result, args, cache, _info) => {
-                        if (!result.updateArticle) return;
-
-                        cache.invalidate("Query", "articles");
-                        cache.invalidate("Query", "article", { id: args.id });
-                    },
-                    deleteArticle: (result, args, cache, _info) => {
-                        if (!result.deleteArticle) return;
-
-                        cache.updateQuery<ArticlesQuery>({ query: ArticlesDocument }, data => {
-                            if (!data) return null;
-
-                            const filteredArticles = data.articles.filter(article => article.id !== args.id);
-                            return { articles: filteredArticles } as ArticlesQuery;
-                        });
-                    },
-                    setPublishArticle: (result, args, cache, _info) => {
-                        if (!result.setPublishArticle) return;
-
-                        cache.updateQuery<ArticlesQuery>({ query: ArticlesDocument }, data => {
-                            if (!data) return null;
-
-                            const resultArticle = data.articles.find(article => article.id === args.id) as Article;
-                            resultArticle.published = args.published as boolean;
-                            return data;
-                        });
-                    },
-                    toggleLike: (result, args, cache, _info) => {
-                        if (!result.toggleLike) return;
-
-                        cache.updateQuery({ query: LikeDocument, variables: { articleId: args.articleId } }, data => {
-                            if (!data) return null;
-
-                            data.like = !data.like;
-                            return data;
-                        });
-                    }
+            }),
+            ssrExchange,
+            /*errorExchange({
+                onError(error) {
+                    if (error.message.includes("Not authenticated"))
+                        Router.push("/editor/login");
                 }
-            }
-        }),
-        ssrExchange,
-        /*errorExchange({
-            onError(error) {
-                if (error.message.includes("Not authenticated"))
-                    Router.push("/editor/login");
-            }
-        }),*/
-        fetchExchange
-    ]
-});
+            }),*/
+            fetchExchange
+        ]
+    }
+
+    if (!___prod___)
+        config.exchanges = [devtoolsExchange, ...config.exchanges!];
+
+    return config;
+};
 
 export default createUrqlClient;
